1c61cb4ae0ebaed91effbe91ab284046ab04ca69
Mohamed Hasim <147535511+mohamedhasim02@users.noreply.github.com>
2023-10-11 19:11:58 +0530

    Update stopwatch.js

--- stopwatch.js
+++ stopwatch.js
@@ -1,54 +1,60 @@
-// JavaScript code for the stop watch
-var stopwatch = document.getElementById("stopwatch");
-var h1 = stopwatch.querySelector("h1");
-var startButton = stopwatch.querySelector("#start");
-var stopButton = stopwatch.querySelector("#stop");
-var resetButton = stopwatch.querySelector("#reset");
-var lapButton = stopwatch.querySelector("#lap");
-var lapsList = stopwatch.querySelector("#laps");
-var startTime;
-var intervalId;
+let startTime;
+let intervalId;
+let isRunning = false;
+
+const stopwatch = document.querySelector('.stopwatch');
+const hoursDisplay = document.getElementById('hours');
+const minutesDisplay = document.getElementById('minutes');
+const secondsDisplay = document.getElementById('seconds');
+const millisecondsDisplay = document.getElementById('milliseconds');
+const startStopButton = document.getElementById('startStop');
+const resetButton = document.getElementById('reset');
+
+startStopButton.addEventListener('click', function () {
+    if (isRunning) {
+        stopTimer();
+        startStopButton.textContent = 'Start';
+    } else {
+        startTimer();
+        startStopButton.textContent = 'Stop';
+    }
+});
+
+resetButton.addEventListener('click', function () {
+    resetTimer();
+});
 
 function startTimer() {
-  startTime = Date.now();
-  intervalId = setInterval(updateTimer, 10);
+    if (!isRunning) {
+        startTime = Date.now() - (startTime || 0);
+        intervalId = setInterval(updateTimer, 10);
+        isRunning = true;
+    }
 }
 
 function stopTimer() {
-  clearInterval(intervalId);
+    if (isRunning) {
+        clearInterval(intervalId);
+        isRunning = false;
+    }
 }
 
 function resetTimer() {
-  stopTimer();
-  h1.textContent = "00:00:00";
-  lapsList.innerHTML = "";
-}
-
-function lapTimer() {
-  var lapTime = formatTime(Date.now() - startTime);
-  var lapItem = document.createElement("li");
-  lapItem.textContent = lapTime;
-  lapsList.appendChild(lapItem);
+    stopTimer();
+    startTime = 0;
+    updateTimer();
+    startStopButton.textContent = 'Start';
 }
 
 function updateTimer() {
-  var elapsedTime = Date.now() - startTime;
-  var formattedTime = formatTime(elapsedTime);
-  h1.textContent = formattedTime;
-}
+    const currentTime = Date.now() - startTime;
+    const hours = Math.floor(currentTime / 3600000);
+    const minutes = Math.floor((currentTime % 3600000) / 60000);
+    const seconds = Math.floor((currentTime % 60000) / 1000);
+    const milliseconds = currentTime % 1000;
 
-function formatTime(time) {
-  var date = new Date(time);
-  var hours = date.getUTCHours().toString().padStart(2, "0");
-  var minutes = date.getUTCMinutes().toString().padStart(2, "0");
-  var seconds = date.getUTCSeconds().toString().padStart(2, "0");
-  var milliseconds = Math.floor(date.getUTCMilliseconds() / 10)
-    .toString()
-    .padStart(2, "0");
-  return `${hours}:${minutes}:${seconds}.${milliseconds}`;
+    hoursDisplay.textContent = hours.toString().padStart(2, '0');
+    minutesDisplay.textContent = minutes.toString().padStart(2, '0');
+    secondsDisplay.textContent = seconds.toString().padStart(2, '0');
+    millisecondsDisplay.textContent = milliseconds.toString().padStart(3, '0');
 }
-
-startButton.addEventListener("click", startTimer);
-stopButton.addEventListener("click", stopTimer);
-resetButton.addEventListener("click", resetTimer);
-lapButton.addEventListener("click", lapTimer);
